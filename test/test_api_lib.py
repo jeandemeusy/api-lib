31d74581ad43057efbdede6b62f3de5ab7d5c1a2
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-06-19 17:35:44 +0200

    skipping some tests in the CI

--- test/test_api_lib.py
+++ test/test_api_lib.py
@@ -1,9 +1,12 @@
+import os
+
 import pytest
 
 from .config.github_api import GithubAPI
 
 
 @pytest.mark.asyncio
+@pytest.mark.skipif(os.getenv("CI", "false") == "true", reason="Skipping on CI")
 async def test_simple_api(gh_api: GithubAPI):
     user = await gh_api.user()
 
@@ -14,6 +17,7 @@ async def test_simple_api(gh_api: GithubAPI):
 
 
 @pytest.mark.asyncio
+@pytest.mark.skipif(os.getenv("CI", "false") == "true", reason="Skipping on CI")
 async def test_api_returns_list(gh_api: GithubAPI):
     repos = await gh_api.repositories("astral-sh")
     assert isinstance(repos, list)
