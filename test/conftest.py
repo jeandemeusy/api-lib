dc90e05e159d980353ed801415982e5cf385e69b
Jean Demeusy <61140535+jeandemeusy@users.noreply.github.com>
2025-06-26 18:42:49 +0200

    feat(api): enhance API functionality and add Flask integration

--- test/conftest.py
+++ test/conftest.py
@@ -1,19 +1,37 @@
+import time
+from multiprocessing import Process
+from test.config.rest_api import run_server
+
 import pytest
 from dotenv import load_dotenv
 
+from api_lib.api_lib import ApiLib
 from api_lib.headers.authorization import Bearer
 
-from .config.github_api import GithubAPI
 from .config.request import RequestClass
 
 load_dotenv()
 
 
 @pytest.fixture
-def gh_api():
-    return GithubAPI("https://api.github.com", Bearer(env_var="GITHUB_TOKEN"))
+def api():
+    return ApiLib("http://127.0.0.1:5001", Bearer("test_token"))
+
+
+@pytest.fixture
+def api_not_reachable():
+    return ApiLib("http://127.0.0.1:5002", Bearer("test_token"))
 
 
 @pytest.fixture
 def request_object():
     return RequestClass("test_value", "path_value")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def rest_server():
+    proc = Process(target=run_server, daemon=True)
+    proc.start()
+    time.sleep(1)  # wait for server to start
+    yield
+    proc.terminate()
